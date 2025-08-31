7f3ba2c8185c580c722da793370a304893500b3a
nishantveersatech <nishant.kumar@veersatech.com>
2025-08-31 21:47:53 +0530

    Added support for customized delimeter

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,8 +2,14 @@ require_relative "test"
 
 def add(numbers_string)
     ans = 0
-    numbers_string = numbers_string.split("\n").join(",")
-    numbers_array = numbers_string.split(",")
+    delimeter=","
+    if numbers_string.start_with?("//")
+        match = numbers_string.match(%r{//(.*?)\n(.*)})
+        delimeter = match[1]
+        numbers_string = match[2]
+    end
+    numbers_string = numbers_string.split("\n").join(delimeter)
+    numbers_array = numbers_string.split(delimeter)
     numbers_array.each do |number|
         ans=ans+number.to_i
     end
@@ -14,3 +20,4 @@ puts run_test(method(:add), "", 0)
 puts run_test(method(:add), "1", 1)
 puts run_test(method(:add), "1,5", 6)
 puts run_test(method(:add),"1\n2,3", 6)
+puts run_test(method(:add),"//;\n1;2", 3)
