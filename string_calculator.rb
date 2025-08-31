c0da8a2a7aed479c965fb33aa3297d75c0f2f1ef
nishantveersatech <nishant.kumar@veersatech.com>
2025-08-31 21:44:38 +0530

    Added support for next line(\n)

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,9 +1,10 @@
 require_relative "test"
 
-def add(numbersString)
+def add(numbers_string)
     ans = 0
-    numbersArray = numbersString.split(",")
-    numbersArray.each do |number|
+    numbers_string = numbers_string.split("\n").join(",")
+    numbers_array = numbers_string.split(",")
+    numbers_array.each do |number|
         ans=ans+number.to_i
     end
     ans
@@ -12,3 +13,4 @@ end
 puts run_test(method(:add), "", 0)
 puts run_test(method(:add), "1", 1)
 puts run_test(method(:add), "1,5", 6)
+puts run_test(method(:add),"1\n2,3", 6)
