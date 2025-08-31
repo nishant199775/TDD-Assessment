9dc2f6db50b983ec039386c856b9115f416d8d65
nishantveersatech <nishant.kumar@veersatech.com>
2025-08-31 22:38:27 +0530

    Added support for multiple delimeter

--- string_calculator.rb
+++ string_calculator.rb
@@ -8,11 +8,15 @@ def add(numbers_string)
         if numbers_string.start_with?("//")
             match = numbers_string.match(%r{//(.*?)\n(.*)})
             delimeter = match[1]
+            modified_numbers = match[2]
             if(delimeter.start_with?("["))
-                delimeter_parts = delimeter.match(/\[(.*?)\]/)
-                delimeter = delimeter_parts[1]
+                delimeter_parts = delimeter.scan(/\[(.*?)\]/)
+                delimeter_parts.each do |part|
+                    modified_numbers = modified_numbers.split(part[0]).join(",")
+                end
+                delimeter = ","
             end
-            numbers_string = match[2]
+            numbers_string = modified_numbers
         end
         numbers_string = numbers_string.split("\n").join(delimeter)
         numbers_array = numbers_string.split(delimeter)
@@ -37,9 +41,11 @@ end
 puts run_test(method(:add), "", 0)
 puts run_test(method(:add), "1", 1)
 puts run_test(method(:add), "1,5", 6)
-puts run_test(method(:add),"1\n2,3", 6)
-puts run_test(method(:add),"//;\n1;2", 3)
-puts run_test(method(:add),"1,-2,-3", RuntimeError.new("negative numbers not allowed -2,-3"))
-puts run_test(method(:add),"1,1001,3", 4)
-puts run_test(method(:add),"1,1000,3", 1004)
-puts run_test(method(:add),"//[**]\n1**2**3",6)
+puts run_test(method(:add), "1\n2,3", 6)
+puts run_test(method(:add), "//;\n1;2", 3)
+puts run_test(method(:add), "1,-2,-3", RuntimeError.new("negative numbers not allowed -2,-3"))
+puts run_test(method(:add), "1,1001,3", 4)
+puts run_test(method(:add), "1,1000,3", 1004)
+puts run_test(method(:add), "//[**]\n1**2**3", 6)
+puts run_test(method(:add), "//[*][%]\n1*2%3", 6)
+puts run_test(method(:add), "//[*][%][#]\n1*2%3#4", 10)
