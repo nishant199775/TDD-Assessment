dd2c987bf686c1ed663d537ba6c748b0dc8d32fd
nishantveersatech <nishant.kumar@veersatech.com>
2025-08-31 21:54:34 +0530

    Added support for raising error for negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,19 +1,33 @@
 require_relative "test"
 
 def add(numbers_string)
-    ans = 0
-    delimeter=","
-    if numbers_string.start_with?("//")
-        match = numbers_string.match(%r{//(.*?)\n(.*)})
-        delimeter = match[1]
-        numbers_string = match[2]
+    begin
+        ans=0
+        delimeter=","
+        negative_numbers = []
+        if numbers_string.start_with?("//")
+            match = numbers_string.match(%r{//(.*?)\n(.*)})
+            delimeter = match[1]
+            numbers_string = match[2]
+        end
+        numbers_string = numbers_string.split("\n").join(delimeter)
+        numbers_array = numbers_string.split(delimeter)
+        numbers_array.each do |number|
+            integer_number = number.to_i
+            if integer_number >= 0 
+                ans=ans+integer_number
+            else
+                negative_numbers.push(integer_number)
+            end
+        end
+        if(negative_numbers.length>0)
+            error =  "negative numbers not allowed #{negative_numbers.join(",")}"
+            raise error
+        end
+        ans
+    rescue => e
+        return e
     end
-    numbers_string = numbers_string.split("\n").join(delimeter)
-    numbers_array = numbers_string.split(delimeter)
-    numbers_array.each do |number|
-        ans=ans+number.to_i
-    end
-    ans
 end
 
 puts run_test(method(:add), "", 0)
@@ -21,3 +35,4 @@ puts run_test(method(:add), "1", 1)
 puts run_test(method(:add), "1,5", 6)
 puts run_test(method(:add),"1\n2,3", 6)
 puts run_test(method(:add),"//;\n1;2", 3)
+puts run_test(method(:add),"1,-2,-3", RuntimeError.new("negative numbers not allowed -2,-3"))
--- test.rb
+++ test.rb
@@ -1,3 +1,7 @@
 def run_test(func, arg, expected_result)
-    return func.call(arg) == expected_result
+    if(func.call(arg).class == RuntimeError && expected_result.class == RuntimeError)
+        return func.call(arg).message == expected_result.message
+    else 
+        return func.call(arg) == expected_result
+    end
 end
